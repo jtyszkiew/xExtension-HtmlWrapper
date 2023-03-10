e3c3b313e57f8b37d176f52787d2a239d9fd3d4f
jtyszkiew <jtyszkiew@gmail.com>
2023-03-10 20:57:49 +0100

    Refactor code, add x_html_wrapper marker class

--- extension.php
+++ extension.php
@@ -3,6 +3,10 @@
 class HtmlWrapperExtension extends Minz_Extension
 {
 
+    const CATEGORY_PREFIX = 'c_';
+    const CATEGORY_ID_PREFIX = 'c_id_';
+    const HTML_WRAPPER_CLASS = 'x_html_wrapper';
+
     public function init()
     {
         $this->registerHook('entry_before_display', array($this, 'wrap'));
@@ -10,15 +14,14 @@ class HtmlWrapperExtension extends Minz_Extension
 
     public function wrap($entry)
     {
-        $domain = $this->get_domain_name($entry->link());
-        $category = $this->get_category_wrapper_name($entry);
+        $classes = $this->getWrapperClasses($entry);
 
-        $entry->_content("<div class=\"$domain $category\">" . $entry->content() . "</div>");
+        $entry->_content("<div class=\"$classes\">" . $entry->content() . "</div>");
 
         return $entry;
     }
 
-    private function get_domain_name($url)
+    private function getDomainName($url)
     {
         // Remove the protocol and path from the URL
         $url = preg_replace('#^(https?://)?(www\.)?([^/]+)(/.*)?$#', '$3', $url);
@@ -27,12 +30,28 @@ class HtmlWrapperExtension extends Minz_Extension
         return str_replace('.', '_', $url);
     }
 
-    private function get_category_wrapper_name($entry)
+    private function getWrapperClasses($entry)
     {
         $category = $entry->feed()->category();
-        $name = str_replace(' ', '_', $category->name());
-        $id = $category->id();
+        $classes = [
+            $this->getDomainName($entry->link()),
+            $this->getCategoryClass($this->getFormattedCategoryName($category->name())),
+            $this->getCategoryIdClass($category->id()),
+            self::HTML_WRAPPER_CLASS
+        ];
 
-        return 'c_' . strtolower($name) . ' c_id_' . $id;
+        return join(' ', $classes);
+    }
+
+    private function getCategoryClass($name) {
+        return self::CATEGORY_PREFIX . strtolower($name);
+    }
+
+    private function getCategoryIdClass($id) {
+        return self::CATEGORY_ID_PREFIX . $id;
+    }
+
+    private function getFormattedCategoryName($name) {
+        return strtolower(str_replace(' ', '_', $name));
     }
 }
